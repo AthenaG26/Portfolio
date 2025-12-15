1d834161fd5d860c91318b69cf294bcae12cb77a
Athena Guarneros <ag022606@gmail.com>
2025-12-15 11:49:59 +0000

    up

---
+++ MMP240/showMenu.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var menu = document.getElementById("top-nav");
+    if (menu.style.top === "200px" || menu.style.top === '') {
+        menu.style.top = "30px";
+    } else {
+        menu.style.top = "200px";
+    }
+}
